c8345987c0c96389c78a1a46774df874d7cb361a
Your Name <you@example.com>
2022-10-11 19:04:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React FrameWork for Client Side Applications.
         </p>
         <a
           className="App-link"
